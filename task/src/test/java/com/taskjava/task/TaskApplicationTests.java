e4d48444ad0f6b9281c60c3e46475aad8a96fe03
afminguela <afminguela@gmail.com>
2025-07-31 11:58:52 +0200

    initial Comit

---
+++ task/src/test/java/com/taskjava/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskjava.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
